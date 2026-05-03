456bae68c007539b1e5b4c7c1104ab7736572ccf
ElderStack <jse54664@gmail.com>
2026-05-02 20:44:54 -0400

    synth library

---
+++ vectorSynth.java
@@ -0,0 +1,20 @@
+import synth.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class vectorSynth extends Synth
+    {
+    public static void main(String[] args)
+        {
+            Synth.run(new vectorSynth(), args);
+        }
+
+    public void setup()
+        {
+
+        }
+
+    }
+
